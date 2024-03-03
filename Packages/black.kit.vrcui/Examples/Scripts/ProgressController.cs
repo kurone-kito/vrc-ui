5d1d42da4cebd5818983094eb5e1b1885c53697f
kurone-kito <krone@kit.black>
2024-03-03 14:55:01 +0900

    feat: added the organisms prefabs of the progress bar

---
+++ Packages/black.kit.vrcui/Examples/Scripts/ProgressController.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using black.kit.vrcui;
+
+namespace black.kit.vrcui.Example
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public sealed class ProgressController : UdonSharpBehaviour
+    {
+#pragma warning disable IDE0044
+        /// <summary>The progress component.</summary>
+        [SerializeField, Tooltip("Specifies the progress component.")]
+        private Progress progress;
+#pragma warning restore IDE0044
+
+        public void Decrement() => progress.Value -= Random.Range(0f, 30f);
+
+        public void Increment() => progress.Value += Random.Range(0f, 30f);
+    }
+}
