4ace2f716c6df9faf14cf854129d71e93238b47c
kurone-kito <krone@kit.black>
2024-05-28 13:09:38 +0900

    feat: improved the image toggle component

--- Packages/black.kit.vrcui/Editor/T.cs
+++ Packages/black.kit.vrcui/Editor/T.cs
@@ -8,12 +8,12 @@ namespace black.kit.vrcui.Editor
             nameof(DETAIL_TOGGLE_WITH_ANIMATION);
         public const string USAGE_COMPONENT = nameof(USAGE_COMPONENT);
         public const string USAGE_EVENT_TOGGLE = nameof(USAGE_EVENT_TOGGLE);
-        public const string USAGE_ICON_TOGGLE_0 = nameof(USAGE_ICON_TOGGLE_0);
         public const string USAGE_ICON_TOGGLE_1 = nameof(USAGE_ICON_TOGGLE_1);
         public const string USAGE_LINK_ANIMATOR = nameof(USAGE_LINK_ANIMATOR);
         public const string USAGE_LINK_TOGGLE = nameof(USAGE_LINK_TOGGLE);
         public const string USAGE_ON_VALUE_CHANGED =
             nameof(USAGE_ON_VALUE_CHANGED);
+        public const string USAGE_REGIST_SPRITES = nameof(USAGE_REGIST_SPRITES);
         public const string USAGE_RUNTIME_ONLY = nameof(USAGE_RUNTIME_ONLY);
         public const string USAGE_SEND_CUSTOM_EVENT =
             nameof(USAGE_SEND_CUSTOM_EVENT);
