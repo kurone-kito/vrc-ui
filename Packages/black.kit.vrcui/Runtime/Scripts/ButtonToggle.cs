c0cccfc813d79cfa02a12106a6e937fb497b98cd
kurone-kito <krone@kit.black>
2025-05-27 01:34:46 +0900

    style: linted

--- Packages/black.kit.vrcui/Runtime/Scripts/ButtonToggle.cs
+++ Packages/black.kit.vrcui/Runtime/Scripts/ButtonToggle.cs
@@ -1,4 +1,4 @@
-﻿using UdonSharp;
+using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
 
--- Packages/black.kit.vrcui/Runtime/Scripts/Progress.cs
+++ Packages/black.kit.vrcui/Runtime/Scripts/Progress.cs
@@ -1,4 +1,4 @@
-﻿using TMPro;
+using TMPro;
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
